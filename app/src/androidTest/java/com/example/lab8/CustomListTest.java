519940d79a86047daa9ae1aed22a9ccd0bd139f4
PolarisStarnor <polarisstarnor@gmail.com>
2023-03-08 17:37:36 -0700

    Added a test set to fail

--- app/src/androidTest/java/com/example/lab8/CustomListTest.java
+++ app/src/androidTest/java/com/example/lab8/CustomListTest.java
@@ -1,2 +1,29 @@
-package com.example.lab8;public class CustomListTest {
+package com.example.lab8;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class CustomListTest {
+
+    private CustomList list;
+    /**
+     * create a mocklist for my citylist
+     * @return
+     *          Returns an empty CustomList for testing.
+     */
+    public CustomList MockCityList(){
+        list = new CustomList(null,new ArrayList<>());
+        return list;
+    }
+
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        assertEquals(list.getCount(),listSize + 1);
+    }
 }
