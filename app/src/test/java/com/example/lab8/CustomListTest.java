1725dbce9aded8b3787feb7571bb8e14914d7236
PolarisStarnor <polarisstarnor@gmail.com>
2023-03-08 17:43:20 -0700

    Update the correct CustomListTest file

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -5,12 +5,31 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 //import org.junit.Before;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 
 public class CustomListTest {
 
+    private CustomList list;
+    /**
+     * create a mocklist for my citylist
+     * @return
+     *          Returns an empty CustomList for testing.
+     */
+    public CustomList MockCityList(){
+        list = new CustomList(null,new ArrayList<>());
+        return list;
+    }
+
+    @Test
+    public void addCityTest(){
+        list = MockCityList();
+        int listSize = list.getCount();
+        list.addCity(new City("Estevan", "SK"));
+        Assert.assertEquals(list.getCount(),listSize + 1);
+    }
 
 }
