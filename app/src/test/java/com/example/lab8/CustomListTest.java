7df146870fd4522f7f1dec03abd199f526f5e597
PolarisStarnor <polarisstarnor@gmail.com>
2023-03-08 18:46:56 -0700

    Adding hasCityTest to fail

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,6 +2,9 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 //import org.junit.Before;
@@ -14,16 +17,49 @@ public class CustomListTest {
 
     private CustomList list;
 
-    public CustomList MockCityList(){
+    public CustomList mockCityList(){
         list = new CustomList(null, new ArrayList<>());
         return list;
     }
 
     @Test
     public void addCityTest() {
-        list = MockCityList();
+        list = mockCityList();
         int listSize = list.getCount();
         list.addCity(new City("Estevan", "SK"));
         assertEquals(list.getCount(), listSize+1);
     }
+
+    @Test
+    void hasCityTest() {
+        list = mockCityList();
+        City city = new City("Toronto", "ON");
+        list.addCity(city);
+        assertTrue(list.hasCity(city));
+        city = new City("Toronton't", "OFF");
+        assertFalse(list.hasCity(city));
+    }
+
+//    @Test
+//    void removeCityTest(){
+//        list = mockCityList();
+//        City city = new City("Toronto", "ON");
+//        list.addCity(city);
+//        list.removeCity(city);
+//        assertEquals(1, citylist.getCities().size());
+//        assertThrows(IllegalArgumentException.class, () -> {
+//            citylist.delete(city);
+//        });
+//    }
+//
+//    @Test
+//    void countCitiesTest() {
+//        CityList citylist = mockCityList();
+//        assertEquals(1, citylist.countCities());
+//        City city = new City("Toronto", "ON");
+//        citylist.add(city);
+//        assertEquals(2, citylist.countCities());
+//        citylist.delete(city);
+//        assertEquals(1, citylist.countCities());
+//    }
 }
