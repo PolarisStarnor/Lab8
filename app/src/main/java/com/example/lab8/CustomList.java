23d02780485fb2d79cdd90917c37bf2f8651a072
PolarisStarnor <polarisstarnor@gmail.com>
2023-03-08 19:01:22 -0700

    Adding countCitiesTest to fail

--- app/src/main/java/com/example/lab8/CustomList.java
+++ app/src/main/java/com/example/lab8/CustomList.java
@@ -63,8 +63,8 @@ public class CustomList extends ArrayAdapter<City> {
         }
         cities.remove(city);
     }
-//
-//    public int countCities() {
-//
-//    }
+
+    public int countCities() {
+
+    }
 }
